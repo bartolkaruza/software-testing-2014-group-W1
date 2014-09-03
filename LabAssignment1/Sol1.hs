e1bc815335e1d1784ae79b835b7e7f9d3eacaf5f
axelpolet89 <axel.polet33@gmail.com>
2014-09-03 23:25:28 +0200

    completed exercises (except 1.4) of chapter 1

--- LabAssignment1/Sol1.hs
+++ LabAssignment1/Sol1.hs
@@ -43,7 +43,7 @@ count c "" = 0
 count c (x:xs) | c == x = 1 + count c xs
 			   | otherwise = count c xs
 				   
---ex1.14 init x performes as right recursion
+--ex1.14 init x performes as right recursion (or we could just pass an integer along with the blowup call)
 blowup :: String -> String
 blowup [] = []
 blowup x = blowup (init x) ++ replicate (length x) (last x)
@@ -61,4 +61,44 @@ isSubStr [] ys = True
 isSubStr xs [] = False
 isSubStr (x:xs) (y:ys) = prefix (x:xs) (y:ys) 
 							|| isSubStr (x:xs) ys
-					 
+
+--ex1.18
+-- 	:t ["test1", "test2"]
+--	["test1", "test2"] :: [[Char]]
+-- 	:t (True, "Test")
+--	(True, "Test") :: (Bool, [Char])
+--	:t [(True, "Test"), (False, "Test")]
+--	[(True, "Test"), (False, "Test")] :: [(Bool, [Char])]
+--	:t ([True, False], "Test")
+-- 	([True, False], "Test") :: ([Bool], [Char])
+--	:t not 					//the inverse operator
+--  not :: Bool -> Bool
+
+--ex1.19
+--  *Sol1> :t head				//returns first element of list
+--  head :: [a] -> a
+--  *Sol1> :t last				//returns last element of list						
+--  last :: [a] -> a
+--  *Sol1> :t init				//returns all elements of list except last
+--  init :: [a] -> [a]
+--  *Sol1> :t fst				
+--  fst :: (a, b) -> a			//returns first element of tuple
+--  *Sol1> :t (++)
+--  (++) :: [a] -> [a] -> [a]	//takes 2 lists and returns them combined as 1 list
+--  *Sol1> :t flip				//takes function in a and processes the arguments flipped. So flip (/) 2 1 returns 0.5 instead of 2
+--  flip :: (a -> b -> c) -> b -> a -> c	
+--  *Sol1> :t flip (++)			//when concatenating 2 lists, elements of the second argument will appended first
+--  flip (++) :: [a] -> [a] -> [a]
+
+--ex1.20
+lengths :: [[a]] -> [Int]
+lengths xs = map length xs
+
+--ex1.21
+sumLengths :: [[a]] -> Int
+sumLengths xs = sum (lengths xs)
+
+--ex1.24
+-- it still works, because the first incorrect ldp call (without argument) automatically triggers
+-- a new correct ldp call through the 3d case of the 'prime' function
+
