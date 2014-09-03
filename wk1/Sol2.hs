16fc77f5cf4e2e5091697abd9e23bae9649e6c72
Remco Hendriks <remco@topnotchdevelopment.nl>
2014-09-03 16:19:30 +0200

    tabs->spaces

--- wk1/Sol2.hs
+++ wk1/Sol2.hs
@@ -1,28 +1,28 @@
 module Sol1 where
 
-	import GS
-	import TAMO
+    import GS
+    import TAMO
 
-	-- 2.13
-	t1   = logEquiv1 (\p -> False) (\p -> not True)
-	t2   = logEquiv1 (\p -> p ==> False) (\p -> not p)
-	t3_1 = logEquiv1 (\p -> p || True) (\p -> True)
-	t3_2 = logEquiv1 (\p -> p && False) (\p -> False)
-	t4_1 = logEquiv1 (\p -> p || False) id
-	t4_2 = logEquiv1 (\p -> p && True) id
-	t5   = logEquiv1 (\p -> p || not p) (\p -> True)
-	t6   = logEquiv1 (\p -> p && not p) (\p -> False)
+    -- 2.13
+    t1   = logEquiv1 (\p -> False) (\p -> not True)
+    t2   = logEquiv1 (\p -> p ==> False) (\p -> not p)
+    t3_1 = logEquiv1 (\p -> p || True) (\p -> True)
+    t3_2 = logEquiv1 (\p -> p && False) (\p -> False)
+    t4_1 = logEquiv1 (\p -> p || False) id
+    t4_2 = logEquiv1 (\p -> p && True) id
+    t5   = logEquiv1 (\p -> p || not p) (\p -> True)
+    t6   = logEquiv1 (\p -> p && not p) (\p -> False)
 
-	-- 2.15
-	-- ?
+    -- 2.15
+    -- ?
 
-	-- 2.20
-	p1 = logEquiv2 (\p q -> not p ==> q) (\p q -> p ==> not q)
-	p2 = logEquiv2 (\p q -> not p ==> q) (\p q -> q ==> not p)
-	p3 = logEquiv2 (\p q -> not p ==> q) (\p q -> not q ==> p)
-	p4 = logEquiv3 (\p q r -> p ==> (q ==> r)) (\p q r -> q ==> (p ==> r))
-	p5 = logEquiv3 (\p q r -> p ==> (q ==> r)) (\p q r -> (p ==> q) ==> r)
-	p6 = logEquiv2 (\p q -> (p ==> q) ==> p) (\p q -> p)
-	p7 = logEquiv3 (\p q r -> (p || q) ==> r) (\p q r -> (p ==> r) || (q ==> r))
+    -- 2.20
+    p1 = logEquiv2 (\p q -> not p ==> q) (\p q -> p ==> not q)
+    p2 = logEquiv2 (\p q -> not p ==> q) (\p q -> q ==> not p)
+    p3 = logEquiv2 (\p q -> not p ==> q) (\p q -> not q ==> p)
+    p4 = logEquiv3 (\p q r -> p ==> (q ==> r)) (\p q r -> q ==> (p ==> r))
+    p5 = logEquiv3 (\p q r -> p ==> (q ==> r)) (\p q r -> (p ==> q) ==> r)
+    p6 = logEquiv2 (\p q -> (p ==> q) ==> p) (\p q -> p)
+    p7 = logEquiv3 (\p q r -> (p || q) ==> r) (\p q r -> (p ==> r) || (q ==> r))
 
-	
+    
