9ad1e993bad02166f5ffa6280ce2b5f6929f26a6
axelpolet89 <axel.polet33@gmail.com>
2014-09-08 22:13:13 +0200

    bonus up until exercise 4 (tiger/lady)

--- LabAssignment1/Lab1Bonus_Axel.hs
+++ LabAssignment1/Lab1Bonus_Axel.hs
@@ -1,5 +1,7 @@
-module Lab1Bonus
+module Lab1Bonus_Axel
 where
+
+import Lab1Bonus
 
 foldrLen :: [a] -> Int
 foldrLen xs = foldr (\x y -> y + 1) 0 xs
@@ -18,7 +20,25 @@ foldrFilter :: (a -> Bool) -> [a] -> [a]
 foldrFilter f xs = foldr (\x y -> if f x then x : y else y) [] xs
 
 foldrPlus :: [a] -> [a] -> [a]
-foldrPlus xs ys = foldr (\x y -> x:y) ys xs 		--last ys is identity element, that is appended in the last fold (as the last x:y)(
+foldrPlus xs ys = foldr (\x y -> x:y) ys xs 		--last ys is identity element, that is appended in the last fold (as the last x:y)
 
 foldrReverse :: [a] -> [a]
 foldrReverse xs = foldr (\x y -> y ++ [x]) [] xs
+
+foldlReverse :: [a] -> [a]
+foldlReverse xs = foldl (\x y -> y:x) [] xs
+
+--foldr, not foldl
+--foldl will build the entire expression by iterating through the whole list (and then evaluate everything backwards). Because the last item of the list is evaluated first (the outermost item of the expression foldl has built. If the list is infinite, this will eventually cause a stack overflow
+--foldr applies the given function to the next value in each iteration and the result of the following folds. Because it runs forward it doesn't have to go through the entire list first to create results and therefore may work on infinite lists.
+
+--bonus ex4
+sign3, sign4 :: (Creature, Creature) -> Bool
+sign3 (x,y) = x == Lady || y == Lady
+sign4 (x,y) = x == Tiger && (y == Tiger || y == Lady)
+
+solution2 :: [(Creature,Creature)]
+solution2 = [ (x,y) | x <- [Lady,Tiger],
+					  y <- [Lady,Tiger],
+					  sign3(x,y) == sign4(x,y) ]			  
+--this leads to [(Tiger,Lady)]
