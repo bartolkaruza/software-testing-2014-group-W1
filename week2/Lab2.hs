23b1166ab1d244e0a357dc9ef25a8780aedcbd5c
rperz <ROBIN352@LPC-LR9HGYNP.internal.towerswatson.com>
2014-09-13 15:29:34 +0200

    changed some stuff, started on bonus exercise

--- week2/Lab2.hs
+++ week2/Lab2.hs
@@ -26,9 +26,9 @@ isPermutation all@(x:xs) ys = if ((length all) /= (length ys)) then False
 							  else isPermutation xs (delete x ys)
 	
 
--- we can check these outcomes by testing if the list of permutations is of length 2^n where n = length of input
+-- we can check these outcomes by testing if the list of permutations is of length n! where n = length of input
 perms :: [a] -> [[a]]
-perms [] = []
+perms [] = error "empty list"
 perms [x] = [[x]]
 perms all@(x:xs) =  (concatMap (insert' x)) (perms xs)
 
@@ -38,13 +38,26 @@ insert' x [y]= [[x,y], [y,x]]
 insert' x all@(y:ys) = (x:all):(map (y:) (insert' x ys))
 
 isDerangement ::  [Int] -> [Int] -> Bool
-
 isDerangement xs ys = if isPermutation xs ys then and $ (zipWith (/=) xs ys)
 					  else False
-
+			  
+-- here we need to do something to make sure that the input is a natural number
 deran :: [Int] -> [[Int]]
 deran xs = filter (isDerangement xs $) (perms xs)
 
+-- we could test if there are duplicates in the list
+-- we could test if the count is the same
+-- we could test if each element in the old list is in the new list
+-- Lists:
+-- [1,2,3,4,5]
+-- [1,1,2,3,4]
+-- [1,4,5,6,7]
+-- [-1,2,3,4,5]
+-- [,1,2,3,4]
 
+--rndDeran :: [Int] -> [Int]
+--rndDeran xs = (deran xs) !! randomNr
+--			where randomNr = randoms (2^(length xs)) :: Int
+			
 
 
