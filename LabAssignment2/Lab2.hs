d766fc23d4e0158636de824b9c81455d13dd6d28
axelpolet89 <axel.polet33@gmail.com>
2014-09-16 20:55:33 +0200

    Ex. 7: + deranTestProp1 (test for lengths), deranTestProp2 (test for inequality of list elements)

--- LabAssignment2/Lab2.hs
+++ LabAssignment2/Lab2.hs
@@ -24,10 +24,6 @@ triangle a b c | a == b && a == c = Equilateral
 triangleTest :: [(Integer, Integer, Integer)] -> Shape -> Bool
 triangleTest [] s = True
 triangleTest (x:xs) s = (triangle (eF x) (eS x) (eL x) == s) && triangleTest xs s
-
---generate rectangular triangles			
-genRectTriangles = [ (a,b,c) | c <- [1..100], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]  
-
 --extract element from triple
 eF :: (a, b, c) -> a
 eF (a,_,_) = a
@@ -36,9 +32,11 @@ eS (_,b,_) = b
 eL :: (a, b, c) -> c
 eL (_,_,c) = c
 
+--generate rectangular triangles			
+genRectTriangles = [ (a,b,c) | c <- [1..100], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]  
 
---Ex.2: 0.5 hours
--- a does not derive 'Ord a', otherwise we could compare the sorted lists isPermutation xs ys = (sort xs) == (sort ys)
+
+--Ex.2: isPermutation function (0.5 hours)
 isPermutation :: Eq a => [a] -> [a] -> Bool
 isPermutation [] [] = True
 isPermutation (x:xs) ys = 
@@ -46,10 +44,11 @@ isPermutation (x:xs) ys =
 		then isPermutation (filter (/=x) xs) (filter (/=x) ys) 
 		else False
 		
---Ex.3: 1 hour
---test function
+		
+--Ex.3 Tests for isPermutation  (1 hour)
 testPerm :: ([Int], [Int]) -> Bool
 testPerm xs = isPermutation (fst xs) (snd xs)
+
 --the reverse is always a permutation
 validPerm :: ([Int], [Int])
 validPerm = ([x | x <- [1..100]], [y | y <- reverse [1..100]])
@@ -59,14 +58,15 @@ invalidPerm = ([x | x <- [1..100]], [y | y <- [1..99]])
 --different start elements
 invalidPerm' :: ([Int], [Int])
 invalidPerm' = ([x | x <- [1..100]], [y | y <- [2..101]])
-
 -- if the input contains no duplicates, this results in a stronger precondition for the test (stronger reqs on input), so the set of tests will be smaller
 
---Ex.4: 2 hours
+
+--Ex.4: Permutation generation (2 hours)
 perms :: Eq a => [a] -> [[a]]
 perms [] = [[]]
 perms xs = [(x:ys) | x <- xs, ys <- perms (delete x xs)]
---example [1,2,3] 
+
+--example [1,2,3]:
 --x <- xs leads to lists results in lists starting with 1, 2 or 3
 --ys <- perms (delete x xs) results to permutations of the remaining list which are then appended to the first list
 --all the combinations of first elements and remaining elements result in all the permutations
@@ -79,18 +79,20 @@ fact :: Int -> Int
 fact 1 = 1
 fact n = n * fact(n-1)
 
---Ex. 5: 1 hour
+
+--Ex.5: isDerangement function (1 hour)
 isDerangement :: Eq a => [a] -> [a] -> Bool
 isDerangement xs ys = if isPermutation xs ys
 					then and (zipWith (/=) xs ys)
 					else False
 
---Ex. 6: 0.5 hours
+					
+--Ex.6: derangement generation (0.5 hours)
 deran :: Eq a => [a] -> [[a]]
 deran xs = filter (isDerangement xs) (perms xs)
 
---Ex. 7: 1 hour
---test function
+
+--Ex.7: tests for isDerangement (1 hour)
 testDeran :: ([Int], [Int]) -> Bool
 testDeran xs = isDerangement (fst xs) (snd xs)
 --the reverse is always a derangement
@@ -100,7 +102,18 @@ validDeran = ([x | x <- [1..100]], [y | y <- reverse [1..100]])
 invalidDeran :: ([Int], [Int])
 invalidDeran = ([x | x <- [1..100]], [y | y <- 1:reverse [2..100]])
 
---Ex. 8 Bonus: 2 hours
+--Ex.7 continued: testable properties for isDerangements
+deranProp1 :: Eq a => [a] -> [a] -> Bool
+deranProp1 xs ys = (isDerangement xs ys) == ((length xs) == (length ys))
+
+deranProp2 :: Eq a => [a] -> [a] -> Bool
+deranProp2 xs ys = (isDerangement xs ys) == (listsNonEqual xs ys)
+
+listsNonEqual :: Eq a => [a] -> [a] -> Bool
+listsNonEqual [] [] = True
+listsNonEqual (x:xs) (y:ys) = (x /= y) && (listsNonEqual xs ys)
+
+--Ex.8 Bonus1 (2 hours)
 arbDeran :: (Show a, Eq a) => [a] -> IO ()
 arbDeran xs = do
 	rnd <- getRndIndex (deran xs)
