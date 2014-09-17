7b3413007357f9f9beeb115369740e38f5beb4a6
Bartol Karuza <me@bartolkaruza.com>
2014-09-17 14:30:48 +0200

    contradiction, tautology + 1 test, 3 tests for entails

--- week3/Week3-exercises.hs
+++ week3/Week3-exercises.hs
@@ -4,3 +4,34 @@ module Main where
 
 	main :: IO ()
 	main = return ()
+
+	contradiction :: Form -> Bool
+	contradiction f = not (satisfiable f)
+
+	tautology :: Form -> Bool
+	tautology f = all (\ v -> eval v f) (allVals f)
+
+	testTautology :: Bool
+	testTautology = tautology (parseSingle "+(1 -1)")
+
+	---- logical entailment
+	entails :: Form -> Form -> Bool
+	entails _ _ = True
+
+	-- {p} |# (p ∧ q)
+	testEntails1 :: Bool
+	testEntails1 = not (entails (parseSingle "1") (parseSingle "*(1 2)"))
+
+	-- {p, q} |= (p ∧ q)
+	testEntails2 :: Bool
+	testEntails2 = entails (parseSingle "(1 2)") (parseSingle "*(1 2)")
+
+	-- {p} |= (p ∨ q)
+	testEntails3 :: Bool
+	testEntails3 = entails (parseSingle "(1)") (parseSingle "+(1 2)")
+
+	---- logical equivalence
+	--equiv :: Form -> Form -> Bool
+
+	parseSingle :: String -> Form
+	parseSingle s = head (parse s)
