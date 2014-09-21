f4ea27e1005f1117f7eb5387261233d6592fd75d
axelpolet89 <axel.polet33@gmail.com>
2014-09-21 17:46:13 +0200

    changed cnf implementation to handle [Form] when applying distribution laws. Result seems to be in CNF, however it contains nested conjunction and disjunctions, should be simplified i think?

--- week3/Week3-exercises.hs
+++ week3/Week3-exercises.hs
@@ -39,18 +39,32 @@ module Main where
 ---------------------------------------
 --Ex.2 CNF  (2 hours)
 
+	--preconditions: input is in NNF
 	cnf :: Form -> Form
-	cnf (Neg (Prop x)) = Neg (Prop x)
 	cnf (Prop x) = Prop x
+	cnf (Neg (Prop x)) = Neg (Prop x)
 	cnf (Cnj fs) = Cnj(map cnf fs)
-	cnf (Dsj [f1,f2]) = dist (cnf f1) (cnf f2)
+	cnf (Dsj []) = Dsj[]				--distList cannot handle empty list
+	cnf (Dsj fs) = distList (map cnf fs)
 
-	--is this correct??
+	--apply distribution laws on list of forms
+	--precondition: every form is in cnf
+	distList :: [Form] -> Form
+	distList [] = error "should not come here"
+	distList [f] = f
+	distList (f:fs) = dist f (distList fs)
+	
+	--precondition: f1 and f2 are in cnf
 	dist :: Form -> Form -> Form
-	dist (Cnj[f1,f2]) fs = Cnj([(dist f1 fs),(dist f2 fs)])
-	dist fs (Cnj[f1,f2]) = Cnj([(dist fs f1),(dist fs f2)])
-	dist f1 f2 = Dsj([f1,f2])
-		
+	dist (Cnj f1) f2 = Cnj(map (\f -> (dist f f2)) f1)
+	dist f1 (Cnj f2) = Cnj(map (\f -> (dist f1 f)) f2)
+	dist f1 f2 = Dsj[f1, f2]
+	
+	--precondition: input is in cnf
+	simplify :: Form -> Form
+	simplify 
+			
+	--main conversion function
 	convertCNF :: Form -> Form
 	convertCNF f = cnf (nnf (arrowfree (f)))
 	
@@ -58,3 +72,38 @@ module Main where
 	test2 = (Neg (Dsj([p, (Neg q)])))
 	test3 = (Neg (Cnj([Neg(p), Neg(q)])))
 	test4 = Equiv (Impl p q) (Impl (Neg q) (Neg p))
+	--distribution laws
+	test5 = Dsj[(Cnj[p,q]), r]
+	test6 = Dsj[p, (Cnj[q,r])]
+	test7 = Impl (Cnj[(Neg(p)), (Neg q)]) p
+	
+	
+-------------------------------------------
+--Ex.3 CNF test
+
+	testCNFMain :: Int -> IO ()
+	testFormsOnCNF n = do 
+	fs <- getRandomFs n
+	test n testComb (map convertCNF fs)
+
+	testComb :: Form -> Bool
+	testComb f = (testAF f) && (testNNF f)
+
+	testAF :: Form -> Bool
+	testAF (Prop x) = True
+	testAF (Neg f) = testAF f
+	testAF (Cnj fs) = and (map testAF fs)
+	testAF (Dsj fs) = and (map testAF fs)
+	testAF (Impl f1 f2) = False
+	testAF (Equiv f1 f2) = False
+	
+	testNNF :: Form -> Bool
+	testNNF (Prop x) = True
+	testNNF (Neg (Prop x)) = True		--only negation that is allowed
+	testNNF (Neg f) = False				--for every cnj/dsj within negation
+	testNNF (Cnj fs) = and (map testNNF fs)
+	testNNF (Dsj fs) = and (map testNNF fs)
+	
+	{- 
+	we should test each disjunction within a conjunction, however the output of convertCNF contains nested conjunctions/disjunctions --> we need to simplify the CNF result first...
+	-}
