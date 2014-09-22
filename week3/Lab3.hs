a50df2a9cb7d7609875901bfd0207cee890e26c1
rperz <rperz86@gmail.com>
2014-09-22 20:49:46 +0200

    extra zooi voor CNF conversie toegevoegd

--- week3/Lab3.hs
+++ week3/Lab3.hs
@@ -25,3 +25,14 @@ main s = print (showLst [  ( convert f) | f<- (parse  s)])
 -- convert
 convert :: Form -> Form
 convert f = nnf  (arrowfree f)
+
+cnf :: Form -> Form 
+cnf (Prop x) = Prop x
+cnf (Cnj fs) = Cnj (map cnf fs)
+cnf (Dsj (fs:gs)) = dist (cnf fs) (cnf gs)
+
+dist :: Form -> Form -> Form
+dist (Cnj fs) gs = Cnj (map ((flip dist) gs) fs)
+dist fs (Cnj gs) = Cnj (map (dist fs) gs)
+dist fs gs = Dsj (fs:[gs])
+-- we need to test that the 
