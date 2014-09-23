1e877de33a25e7059f983e3971f15b8bf084b92a
Bartol Karuza <me@bartolkaruza.com>
2014-09-23 21:28:31 +0200

    added test
    Signed-off-by: Bartol Karuza <me@bartolkaruza.com>

--- week3/Week3-exercises.hs
+++ week3/Week3-exercises.hs
@@ -48,15 +48,36 @@ module Main where
 	parseSingle :: String -> Form
 	parseSingle s = head (parse s)
 
+	-- Ex 2
+
+	prepareCnf :: Form -> Form
+	prepareCnf f = nnf (arrowfree f)
+
 	cnf :: Form -> Form
 	cnf (Prop x) = Prop x
+	cnf (Neg (Prop x)) = (Neg (Prop x))
 	cnf (Cnj [x,y]) = Cnj [(cnf x),(cnf y)]
 	cnf (Dsj [x,y]) = dist (cnf x) (cnf y)
-	--cnf 
 
 	dist :: Form -> Form -> Form
 	dist (Cnj [x,y]) z = Cnj [(dist x z),(dist y z)]
 	dist x (Cnj [y,z]) = Cnj [(dist x y),(dist x z)]
 	dist x y = Dsj [x,y] 
 
+	-- Ex 3
+	isCnf :: Form -> Bool
+	isCnf (Prop x) = True
+	isCnf (Neg (Prop x)) = True
+	isCnf (Neg _) = False	-- Negation not of a property
+	isCnf (Dsj ((Cnj x):xs)) = False -- Conjunction below a Disjunction
+	isCnf (Dsj (x:xs)) = isCnf x && isCnf(Dsj xs)
+	isCnf (Dsj fs) = foldr (\x y -> (isCnf x) && y) True fs
+	isCnf (Cnj fs) = foldr (\x y -> (isCnf x) && y) True fs
+	isCnf (Impl _ _ ) = False
+	isCnf (Equiv _ _ ) = False
+
+	testCnf :: Int -> IO ()
+	testCnf n = do
+		fs <- getRandomFs n
+		test n isCnf (map cnf (map prepareCnf fs))
 	--dist :: Form -> Form -> Form
