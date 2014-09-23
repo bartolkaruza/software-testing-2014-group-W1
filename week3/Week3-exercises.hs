01d865169974eafa9cde9a8c36c6c0884dc7fccb
Bartol Karuza <me@bartolkaruza.com>
2014-09-23 20:39:12 +0200

    added cnf
    Signed-off-by: Bartol Karuza <me@bartolkaruza.com>

--- week3/Week3-exercises.hs
+++ week3/Week3-exercises.hs
@@ -5,6 +5,8 @@ module Main where
 	main :: IO ()
 	main = return ()
 
+	-- Ex1
+
 	contradiction :: Form -> Bool
 	contradiction f = not (satisfiable f)
 
@@ -16,7 +18,7 @@ module Main where
 
 	---- logical entailment
 	entails :: Form -> Form -> Bool
-	entails _ _ = True
+	entails f j = tautology (Impl f j)
 
 	testEntails :: Bool
 	testEntails = (testEntails1) && (testEntails2) && (testEntails3)
@@ -27,14 +29,34 @@ module Main where
 
 	-- {p, q} |= (p ∧ q)
 	testEntails2 :: Bool
-	testEntails2 = entails (parseSingle "(1 2)") (parseSingle "*(1 2)")
+	testEntails2 = entails (parseSingle "*(1 2)") (parseSingle "*(1 2)")
 
 	-- {p} |= (p ∨ q)
 	testEntails3 :: Bool
-	testEntails3 = entails (parseSingle "(1)") (parseSingle "+(1 2)")
+	testEntails3 = entails (parseSingle "1") (parseSingle "+(1 2)")
 
 	---- logical equivalence
-	--equiv :: Form -> Form -> Bool
+	equiv :: Form -> Form -> Bool
+	equiv f j = tautology (Equiv f j)
+
+	testEquiv1 :: Bool
+	testEquiv1 = equiv (parseSingle "1") (parseSingle "1")
+
+	testEquiv2 :: Bool
+	testEquiv2 = not (equiv (parseSingle "1") (parseSingle "-1"))
 
 	parseSingle :: String -> Form
 	parseSingle s = head (parse s)
+
+	cnf :: Form -> Form
+	cnf (Prop x) = Prop x
+	cnf (Cnj [x,y]) = Cnj [(cnf x),(cnf y)]
+	cnf (Dsj [x,y]) = dist (cnf x) (cnf y)
+	--cnf 
+
+	dist :: Form -> Form -> Form
+	dist (Cnj [x,y]) z = Cnj [(dist x z),(dist y z)]
+	dist x (Cnj [y,z]) = Cnj [(dist x y),(dist x z)]
+	dist x y = Dsj [x,y] 
+
+	--dist :: Form -> Form -> Form
