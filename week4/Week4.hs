33a4cd734e245e07a1b2d57546abf8c4c15935d8
rperz <rperz86@gmail.com>
2014-09-28 19:36:09 +0200

    Bonus exercise and exercises until 5
    Exercise 5 is missing a cast, but I don't understand how to do this yet.

--- week4/Week4.hs
+++ week4/Week4.hs
@@ -3,6 +3,7 @@ module Week4 where
 import SetOrd
 import System.Random
 import Test.QuickCheck
+import Data.List
 --gen :: [Int]
 --gen = do 
 --      g <- newStdGen
@@ -10,8 +11,14 @@ import Test.QuickCheck
 --      print . tenPseudorandomNumbers n	  
 
 -- invariant: no duplicates
-checkDuplicates = quickCheck (\x ->  do s <- getRandomSet
-                                     not (inSet x s) || (inSet x s && not (inSet x (deleteSet x s))))
+dupTest = do 
+          s <- getRandomSet 
+          deepCheck (\x -> checkDuplicates x s) 
+ 
+checkDuplicates :: Int -> Set Int -> Bool
+checkDuplicates x s = not (inSet x s) || (inSet x s && not (inSet x (deleteSet x s)))
+
+deepCheck someProp = quickCheckWith stdArgs { maxSuccess = 5000 } someProp
 
 getRandomSet :: IO (Set Int)
 getRandomSet = do 
@@ -29,13 +36,42 @@ getRandomInt = do
   let [n] = take 1 $ (randomRs (0, 100) g :: [Int])
   return n
   
---getRandomIntList :: Int -> IO [Int]
---getRandomIntList n = do
---  g <- getStdGen
---  take n (randoms g :: Int)
+setIntersection s t = setIntersect s t emptySet
 
---getRandomSet = do 
---               let xs = test
---               print $ list2set xs 
-				   
-				 
+setUnion s t = joinSets s t emptySet 
+ 
+setDifference s t = setDiff s t s
+
+-- helper intersect 
+setIntersect s all@(Set (t:ts)) u  = if inSet t s then setIntersect s (Set ts) (insertSet t u)
+                                        else setIntersect s (Set ts) u
+setIntersect _ _ u = u
+
+-- helper union
+joinSets s all@(Set (t:ts)) u  = joinSets s (Set ts) (insertSet t u)
+joinSets _ _ u = u
+
+-- helper diff	   				 
+setDiff s (Set (t:ts)) u = setDiff s (Set ts) (deleteSet t u)
+setDiff _ _ u = u
+
+type Rel a = [(a,a)]
+
+infixr 5 @@
+(@@) :: Eq a => Rel a -> Rel a -> Rel a
+r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
+
+trClos :: Ord a => Rel a -> Rel a
+trClos r = transClosure r r 
+
+
+-- I need to check if the set difference of the new relation with the old relation is the empty set
+transClosure :: Ord a => Rel a -> Rel a -> Rel a
+transClosure ra@(r:rs) sa@(s:ss) = if setDifference (Set (ra @@ sa)) (Set ra) == emptySet && setDifference (Set (ra @@ sa)) (Set sa) == emptySet then setUnion (Set ra) (Set sa)
+                                   else transClosure ra (ra @@ sa) 
+								   
+---- BONUS
+{- if we define the value that we put in as v then we can see that the formula is (x+v/x)/2. When x=sqrt(v) then v/x=x, so in that case 
+   the output is (x+x)/2=x, which makes the if statement get into the if clause instead of the else clause and the program terminates. 
+   As written earlier, in this case x=sqrt(v).
+  -}
