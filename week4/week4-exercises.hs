d2c0f0e90a82544720c2dc4381f5c74736135feb
axelpolet89 <axel.polet33@gmail.com>
2014-09-29 23:27:19 +0200

    Up until Ex.7

--- week4/week4-exercises.hs
+++ week4/week4-exercises.hs
@@ -10,19 +10,29 @@ import Test.QuickCheck
 
 {-
 	Ex.3 - Random set generation
-	(1 hour)
+	(0.5 hour)
 -}
 getRandomSet :: IO ()
 getRandomSet = do 
 	gen <- newStdGen
-	print $ list2set (getRndList 100 gen)
-	
-getRndList :: Int -> StdGen -> [Int]
-getRndList n g = take n (randomRs (0, 1000) g :: [Int])
+	print $ getRndSet 100 1000 gen
 
-qcSetContents = verboseCheck ((\s -> list2set s == list2set s) :: [Int] -> Bool)
+getRndSet :: Int -> Int -> StdGen -> Set Int
+getRndSet n r g = list2set $ getRndList n r g
 
---qcSetUnique = verboseCheck ((\s -> (length ( nub ( list2set s))) == (length (list2set s))) :: [Int] -> Bool)
+-- this function is also used in Ex.7
+getRndList :: Int -> Int -> StdGen -> [Int]
+getRndList n r g = take n (randomRs (0, r) g :: [Int])
+
+qcEqualSets = ((\s -> list2set s == list2set s) :: [Int] -> Bool)
+qcUniqueElems = ((\s -> uniqueList $ list2set s):: [Int] -> Bool)
+
+uniqueList :: Eq a => Set a -> Bool
+uniqueList (Set []) = True
+uniqueList (Set xs) = length (nub xs) == length xs  
+
+--quickCheck qcEqualSets --> 100 tests passed!
+--quickCheck qcUniqueElems --> 100 tests passed!
 
 {-
 	Ex.4 Set operations
@@ -49,7 +59,9 @@ intersectSet (Set (x:xs)) set2 = if inSet x set2
 qcIntersectSet = ((\s t -> isIntersection (list2set s) (list2set t)) :: [Int] -> [Int] -> Bool)
 					
 isIntersection :: (Ord a, Eq a) => Set a -> Set a -> Bool
-isIntersection set1 set2 = (subSet s3 set1) && (subSet s3 set2) && (matchElems set1 set2 s3)
+isIntersection set1 set2 = (subSet s3 set1) && 
+						   (subSet s3 set2) && 
+						   (matchElems set1 set2 s3)
 							where s3 = intersectSet set1 set2
 							
 matchElems :: (Ord a, Eq a) => Set a -> Set a -> Set a -> Bool
@@ -100,11 +112,14 @@ infixr 5 @@
 r @@ s = nub [(x,z) | (x,y) <- r, (w,z) <- s, y == w]
 
 trClos :: Ord a => Rel a -> Rel a
-trClos r = srtRel (r ++ trClos' r r)					
+trClos r = nub (srtRel (r ++ trClos' r r))
 
+--regarding the if-check: the given r2 should not contain elements also present in r', this means the @@ function is bottomed out, this can happen with symmetrical relations
 trClos' :: Ord a => Rel a -> Rel a -> Rel a
 trClos' r1 [] = []
-trClos' r1 r2 = r' ++ (trClos' r1 r')
+trClos' r1 r2 = if((length [ r | r <- r2, not (elem r r') ] > 0)) 
+				then r' ++ (trClos' r1 r')
+				else []
 				where r' = r1 @@ r2
 
 --sort relations by first element
@@ -116,6 +131,7 @@ test3 = [(1,2),(2,3),(3,4)]
 
 {- 
 	Ex.6 Hspec specification
+	(0.5 hour)
 -}
 
 main :: IO ()
@@ -128,3 +144,38 @@ main = hspec $ do
 		trClos [(1,2),(2,3),(3,4)] `shouldBe` [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]
 		
 	  --it "returns a empty list when given an empty list"
+	  
+{-
+	Ex.7 Testing trClos
+	(1.5 hours)
+-}
+
+-- for each pair in r, check if it is transitive with any other pair it should be transitive with
+isTransitive :: Ord a => Rel a -> Bool
+isTransitive [] = True
+isTransitive r = and [ hasTrans (t,s) r | (t,s) <- r ]
+
+-- for (x,y) in r match on (u,v) in r so that y == u, then look for an (x,v)
+hasTrans :: Ord a => (a,a) -> Rel a -> Bool
+hasTrans (x,y) r = and [ elem (x,v) r | (u,v) <- r, y == u]
+
+-- check if R is subset of trClos of R
+subRel :: Ord a => Rel a -> Rel a -> Bool
+subRel set1 set2 = and [ elem (x,y) set2 | (x,y) <- set1 ]
+
+-- main test function
+testClosureTr :: Ord a => Rel a -> Bool
+testClosureTr r = isTransitive r' && subRel r r'
+				where r' = trClos r
+
+-- own implementation: random relations
+testTrClos :: IO()
+testTrClos = do 
+	gen <- newStdGen
+	print $ testClosureTr $ getRandomR gen
+
+getRandomR :: StdGen -> [(Int, Int)]
+getRandomR g = zip (getRndList 100 20 g) (getRndList 100 20 g)
+
+--quickCheck function (generate two Int lists, zip them to get a Int relation [(Int, Int)]
+qcTrClos = ((\x y -> testClosureTr (zip x y)) :: [Int] -> [Int] -> Bool)
