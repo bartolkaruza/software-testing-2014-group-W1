eab7de9d41a85dcd380c729c18ec05d2ecfab5cd
Bartol Karuza <me@bartolkaruza.com>
2014-09-29 23:34:29 +0200

    till ex 5 WIP
    Signed-off-by: Bartol Karuza <me@bartolkaruza.com>

--- week4/week4-exercises.hs
+++ week4/week4-exercises.hs
@@ -5,6 +5,7 @@ module Main where
     import Test.Hspec
     import SetOrd
     import System.Random
+    import Data.List
 
     main :: IO()
     main = 
@@ -32,25 +33,46 @@ module Main where
                     return (take n (randoms g :: [Int]))
 
 --------------------------------
--- Ex.4
+-- Ex.4 (1.5 hr.)
 
-    testIntersect :: IO()
-    testIntersect = verboseCheck ((\s l -> subSet (intersectSet (Set s) (Set l) ) (Set s) ) :: [Int] -> [Int] -> Bool)
+    testIntersect1 :: IO()
+    testIntersect1 = quickCheck ((\s l -> subSet (intersectSet (list2set s) (list2set l) ) (list2set s) ) :: [Int] -> [Int] -> Bool)
+
+    testIntersect2 :: IO()
+    testIntersect2 = quickCheck ((\s l -> subSet (intersectSet (list2set s) (list2set l) ) (list2set l) ) :: [Int] -> [Int] -> Bool)
 
     intersectSet :: (Ord a) => Set a -> Set a -> Set a
     intersectSet (Set []) _ = Set []
     intersectSet _ (Set []) = Set []
     intersectSet (Set xs) (Set ys) = unionSet (Set(filter (\x -> elem x ys) xs)) (Set(filter (\y -> elem y xs) ys))
 
+    testDifference1 :: IO()
+    testDifference1 = quickCheck ((\s l -> let x = (difference (list2set s) (list2set l)) in not (subSet x (Set s) ) || x == (Set [])) :: [Int] -> [Int] -> Bool)
+
+    testDifference2 :: IO()
+    testDifference2 = quickCheck ((\s l -> let x = (difference (list2set s) (list2set l)) in not (subSet x (Set l) ) || x == (Set [])) :: [Int] -> [Int] -> Bool)
+
     difference :: (Ord a) => Set a -> Set a -> Set a
-    difference set1 set2 = deleteWholeSet set1 set2
-    -- testDeleteWholeSet :: Ord a => Set a -> Set a -> Set a
-    -- testDeleteWholeSet = verboseCheck ((\s l -> not (subSet (Set(s)) (deleteWholeSet (Set(s)) (Set(l))) ) ) :: [Int] -> [Int] -> Bool)
+    difference set1 set2 = deleteWholeSet set2 (deleteWholeSet set1 set2)
 
     deleteWholeSet :: Ord a => Set a -> Set a -> Set a
     deleteWholeSet (Set []) set2 = set2
     deleteWholeSet (Set (x:xs)) set2 = deleteWholeSet (Set(xs)) (deleteSet x set2)
 
-    -- unionSet :: (Ord a) => Set a -> Set a -> Set a 
-    -- unionSet (Set [])     set2  =  set2
-    -- unionSet (Set (x:xs)) set2  = insertSet x (unionSet (Set xs) set2)
+-- Ex.5
+
+    type Rel a = [(a,a)]
+
+    infixr 5 @@
+
+    (@@) :: Eq a => Rel a -> Rel a -> Rel a
+    r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w]
+
+    trClos :: Ord a => Rel a -> Rel a
+    trClos [] = []
+    trClos (x:xs) = nub (x : trClos (trClos ([x]@@xs) ++ xs))
+
+    trClosSingle :: Ord a => Rel a -> Rel a -> Rel a
+    trClosSingle x [] = []
+    trClosSingle x (y:ys) = x ++ trClosSingle x (x @@ [y])
+
