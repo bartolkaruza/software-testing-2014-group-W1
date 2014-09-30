04663d9afb2a2cb55b052b22e1f0a59bdfb9555e
rperz <rperz86@gmail.com>
2014-09-30 20:49:49 +0200

    added bonus

--- week4/week4-exercises.hs
+++ week4/week4-exercises.hs
@@ -1,3 +1,15 @@
 module Main where
 
 import Lab4
+
+
+
+
+
+
+
+---- BONUS
+{- if we define the value that we put in as v then we can see that the formula is (x+v/x)/2. When x=sqrt(v) then v/x=x, so in that case 
+   the output is (x+x)/2=x, which makes the if statement get into the if clause instead of the else clause and the program terminates. 
+   As written earlier, in this case x=sqrt(v).
+  -}
