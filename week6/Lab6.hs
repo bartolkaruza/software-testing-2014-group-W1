2cfa266a571ee3c554de6134b6a3b65227c7cd22
axelpolet89 <axel.polet33@gmail.com>
2014-10-11 19:21:14 +0200

    commented/removed unused imports

--- week6/Lab6.hs
+++ week6/Lab6.hs
@@ -2,11 +2,9 @@ module Lab6
 
 where
 import Data.List
-import Data.Array.IO
-import Control.Monad
 import System.Random
-import Test.QuickCheck
-import Test.QuickCheck.Monadic
+--import Test.QuickCheck
+--import Test.QuickCheck.Monadic
 --import Criterion.Main
 import Week6
 
